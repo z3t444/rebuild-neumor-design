3b4aa0ce2a9ad95c00cc8a6758cbe1d509592487
Zeeta:) <ebdtv4@gmail.com>
2024-03-05 03:27:05 +0700

    <ui>( New Component DropDown and Icons)

--- src/Section/Header/HeaderLayout.jsx
+++ src/Section/Header/HeaderLayout.jsx
@@ -12,9 +12,12 @@ import "./style/HeaderLayout.css";
 function HeaderLayout() {
   return (
     <>
-      <nav className="flex items-center">
+      <nav className="flex items-center montserrat justify-around mt-5">
         {/* for DropDown Menu */}
         <div className="flex items-center gap-5">
+          <div className="logo px-4 py-2 rounded-lg">
+            <h3 className="text-3xl font-bold">N</h3>
+          </div>
           <PagesDrop />
           <ComponentsDrop />
           <SupportDrop />
@@ -29,7 +32,7 @@ function HeaderLayout() {
           </a>
           <a href="/">
             <button className="for-document flex hover:scale-95 duration-300 items-center gap-2 p-3 rounded-lg">
-              <img src={DocumentIcons} alt="" width={25} />
+              <img src={DocumentIcons} alt="" width={20} />
               <h3 className="text-sm font-semibold">Docs v1.0</h3>
             </button>
           </a>
--- src/Section/Header/Hooks/SupportDropDown.jsx
+++ src/Section/Header/Hooks/SupportDropDown.jsx
@@ -1,9 +1,37 @@
-function SupportDropDown() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
+import React, { useState } from "react";
+import DropArrow from '../asset/DropArrow.svg'
 
-export default SupportDropDown;
+const DropdownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative">
+      <button onClick={toggleMenu} className="inline-flex items-center gap-1 justify-center px-4 py-2 text-sm font-semibold text-gray-800 rounded-md focus:outline-none " aria-expanded={isOpen ? "true" : "false"}>
+        Support
+        <img src={DropArrow} alt="" width={20} />
+      </button>
+
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-3 w-24 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+              Option 1
+            </a>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+              Option 2
+            </a>
+            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+              Option 3
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropdownMenu;
