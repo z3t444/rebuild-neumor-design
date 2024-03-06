025859af5c780d7a81f27922ca92c8d7fc78e811
Zeeta:) <ebdtv4@gmail.com>
2024-03-06 11:20:07 +0700

    <style> ( Moving Route href to component style)

--- src/Section/Header/Hooks/SupportDropDown.jsx
+++ src/Section/Header/Hooks/SupportDropDown.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import DropArrow from '../asset/DropArrow.svg'
+import RouteSupport from "../../../components/Header/routes/RouteSupport";
 
 const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,17 +18,8 @@ const DropdownMenu = () => {
 
       {isOpen && (
         <div className="origin-top-right absolute right-0 mt-3 w-24 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-              Option 1
-            </a>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-              Option 2
-            </a>
-            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
-              Option 3
-            </a>
-          </div>
+          {/* Route DropDown */}
+          <RouteSupport/>
         </div>
       )}
     </div>
---
+++ src/components/Header/routes/RoutePages.jsx
@@ -0,0 +1,19 @@
+function RoutePages() {
+  return (
+    <>
+      <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+          Option 1
+        </a>
+        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+          Option 2
+        </a>
+        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">
+          Option 3
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default RoutePages;
